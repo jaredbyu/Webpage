5e5ef6996af8d3d8b7b467e146fb95390e2999af
jaredbyu <jaredatjared@gmail.com>
2020-07-20 10:25:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const Twit = require('twit')
-//const notifier = require([node-notifier]);
-//const open = require([open]);
-//const franc = require([franc])
+
 
 const apikey = '4puMWeFU7uVev8xnTMZLBnYvP'
 const apiSecretKey = '5mFpcNGl7fEUSYhwEStjuFVPVMzIhFXfAd3UmiyoHqSBDJO7dE'
@@ -17,39 +15,12 @@ var T = new Twit({
 
 (async () => {
 
-    // //1. GET RECENT TWEETS
-//      T.get('search/tweets', { q: '#paper since:2020-04-15', count: 100 }, function(err, data, response) {
-//        const tweets = data.statuses
-//        .map(tweet => `LANG: ${franc(tweet.text)} : ${tweet.text}`) //CHECK LANGUAGE
-//        .map(tweet => tweet.text)
-//        .filter(tweet => tweet.toLowerCase().includes('paper'));
-//        console.log(tweets);
-//        document.getElementById('demo').innerHTML = tweets;
-//      })
-
-    // //2. REAL TIME MONITORING USING STREAM (HASHTAG)
      var stream = T.stream('statuses/filter', { track: '#tesla' })
      stream.on('tweet', function (tweet) {
          console.log(tweet.text);
          document.getElementById('demo').innerHTML = 'yeet';
          console.log('------');
      })
-
-    // 3. REAL TIME MONITORING USING STREAM (LOCATION)
-    //var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
-    //var stream = T.stream('statuses/filter', { locations: sanFrancisco })
-    
-    //SHOW NOTIFICATION FOR EACH RECEIVED TWEET
-    //stream.on('tweet', function (tweet) {
-     // console.log(tweet.text);
-      //let url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-
-     // notifier.notify({
-      //  title: tweet.user.name,
-      //  message: tweet.text
       });
 
-      
-    //})
-//})();
-  
+
